51ee80dc3c1018ef81848bfa7554973ffa028b36
baczek123 <bakowskipawel83@o2.pl>
2018-09-07 15:27:57 +0200

    taki dodatek

--- functions.php
+++ functions.php
@@ -5,3 +5,9 @@ function Uczymy_script_enqueue() {
     wp_enqueue_script('customjs', get_template_directory_uri() . '/js/UWP.js', array(), '1.0.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'Uczym_script_enqueue');
+
+function uczymuy_sie_suport() {
+add_theme_support('menus');
+}
+
+add_action('init', 'uczymuy_sie_suport');
