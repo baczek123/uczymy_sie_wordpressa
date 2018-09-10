330da02826d15a25344f6e2d0c1a35a8fdb80478
baczek123 <bakowskipawel83@o2.pl>
2018-09-10 21:12:46 +0200

    Revert "formatter"
    This reverts commit f90eb4d1b9411a547ef030112b6c05bddd4d88d9, reversing
    changes made to 37b5ff6f7f3b612000ca4fbc2136720beee7ea05.

--- index.php
+++ index.php
@@ -3,13 +3,97 @@
     <head>
 		<title>uczymysiewardpressa</title>
 		<meta charset="UTF-8">
+		<!--link do stylow bootstrapowych ktore sa lokalnie na dysku-->
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/css/bootstrap.min.css">
+		<!--link do stylow bootstrapowych ktore sa sciagane gdzies z siei -> bo jest na pocztku https -->
+		<!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"  crossorigin="anonymous"-->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"	integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- wersjonowanie [likow css - https://wordpress.stackexchange.com/a/293495 -->
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/my_css/index.css?ver=<?php echo time(); ?>">
     </head>
     <body>
-			<script href="<?php echo get_template_directory_uri();?>/my_css/index.css?ver=<?php echo time(); ?>"<script>
+		<img src="<?php echo get_template_directory_uri();?>/img/IMAG1741.jpg" width="1400" height="800">
+		<div class="conteiner wrap">
+			<nav class="navbar navbar-dark bg-primary back_nav navbarposition">
+				<button class="btn"><i class="fa fa-home"></i></button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="nav nav-pills nav-justified nav-fill">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Start <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link background" href="#first">O Firmie</a>
+						</li>
+						<li class="nav-item ">
+							<a class="nav-link background" href="#secend">Oferta</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link background" href="#third">Kontakt</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<p>Na Maltę wybrałem się w Kwietniu 2018r. i chciałbym podzielić się z wami moimi przemyśleniami na temat wyprawy na tą piękną wyspę. Planując podróż na Maltę zwłaszcza na własną rękę, warto odpowiednio zaplanować sobie miejsce noclegu.  Zdecydowanie polecam okolice Valetty gdzie kilka miejscowości tak naprawdę tworzy jedno duże miasto i właściwie nie widać przejścia z jednej miejscowości do drugiej. Bardziej przypomina to dzielnice w Polskim mieście. Stolicą Malty jest Valletta miejsce najlepsze na chodzenie wąskimi, zabytkowymi i klimatycznymi uliczkami. Jeśli lubicie chodzić promenadą wzdłuż wybrzeża najlepszym miejscem będzie  St. Julian's.  Jeśli chcecie mieć blisko do najbardziej imprezowej dzielnicy z dużą ilością barów i dyskotek zdecydowanie polecam Paceville. Natomiast typowym portem skąd odpływają wszystkie wycieczki statkami, i jest piękny widok na Vallette ( która jest pięknie oświetlona nocą ) jest Sliema.   
+		Malta jest bardzo małą wyspą i bez problemów można się w ciągu 1,5 godziny przedostać z jednego końca wyspy na drugi. Podróżować można komunikacją autobusową która działa dość sprawnie. Pamiętać tylko trzeba że na Malcie wszystkie przystanki autobusowe są na żądanie i jeśli chcemy żeby autobus się zatrzymał i nas zabrał trzeba pokiwać kierowcy z przystanku. Jeśli jedziemy autobusem trzeba też wcześniej zasygnalizować że chcemy wysiąść. Autobus może się nie zatrzymać jeśli jest już maksymalnie zapełniony ludźmi i nikt nie wysiada. Polecam korzystać z Google Maps które dokładnie pokazuje na jaki przystanek mamy iść, w jaki autobus wsiąść i gdzie się przesiąść żeby dojechać w docelowe miejsce. Polecam kupić tygodniowy bilet na całą komunikację miejską na Malcie i pobliskiej wyspie Gozo, który kosztuje 21 euro. Jeśli decydujecie się na jazdę samochodem pamiętajcie że na Malcie jest ruch Lewostronny, uliczki bardzo wąskie a kierowcy jeżdżą nieostrożnie. Dodam jeszcze, że w nocy jest bardzo ciężko dojechać na lotnisko i najprawdopodobniej będzie trzeba pojechać taksówką, jeśli macie bardzo wczesny wylot. 
+		Jeśli liczycie na to, że polecicie na Maltę i będziecie się wylegiwać na piaszczystej plaży możecie się trochę rozczarować. Zdecydowanie większa część lini brzegowej Malty to skały lub klify. Na Malcie jest tylko kilka piaszczystych plaż i są położone w dość dużej odległości od typowo turystycznych miejscowości. W dzielnicy Pcevile ( imprezowa część Malty ) znajduje się bardzo mała plaża szeroka na jakieś 15 metrów i długa może na jakieś 100 - 150 metrów. Osobiście byłem na trzech plażach Golden Bay, Ghajn Tuffieha, Gnejna Bay, które znajdują się bardzo blisko siebie, łącznie mają kilkaset metrów plaży i znajdują się w bardzo malowniczych miejscach.
+		</p>
+		<img src="<?php echo get_template_directory_uri();?>/img/IMAG1677_wm.jpg" width="840" height="480">
+		<p id="first">  Malta posiada wiele cudownych miejsc bardzo często opisywanych w przewodnikach jako atrakcja wyspy. Jednak miejscem, którre zrobiło na mnie naprawdę duże wrażenie ( a nie jest zbyt popularne i nie jest wymieniane jako główna atrakcja Malty ) jest St. Peters Pool. Dotrzeć można tam jadąc do miejscowoći Marsaxlokk. Jest to wioska rybacka słynąca z kolorowych żżdek rybackich z wymalowanymi oczami na ich burtach. Jednak żeby dojżć do St. Peter?s Pool trzeba opużcić miasteczko i przejżć na drugć czćżć pżżwyspu znajdujćcego sią przy tej miejscowożci. St. Peter?s Pool jest mażć zatoczkć wydrćżonć  w skale gdzie fale w majestatyczny sposżb rozbijajć sić o skaży. Bćdćc w okresie w ktżrym tam byżem, spotkażem w tym miejscu bardzo mażć ilożć turystżw. Niewielu ludzi albo wie albo decyduje sić iżć w to miejsce, co sprawia że w bardzo dużym spokoju można eksplorować tć zatokć. </p>
+		<img src="<?php echo get_template_directory_uri();?>/img/IMAG1684_wm.jpg" width="840" height="480">
+		<p id="secend">Będąc na Malcie warto też odwiedzić poboczne wyspy Gozo i Comino. Ja osobiżcie wybrażem się tam wraz z wycieczkę kupionć w porcie, która gwarantoważa zwiedzanie Wyspy Gozo i wolny czas na Bżćkitnej Lagunie na wyspie Comino. Jednak teraz po tym jak skorzystażem z tej opcji nie polecam jej. Wyspa Gozo jest na tyle ciekawa że warto zwiedzać jć przez caży dzież. Pżynćć wraz z wycieczkć statkiem, po dopłynićęciu na wyspćę jesteżmy wiezieni z jednego miejsca na drugie podczas trzy godzinnej objazdżwki. W ten sposżb widzimy najwićksze atrakcje Gozo jednak sć tam miejsca, w ktżrych lepiej spćdzić wićcej czas. Do niedawna najbardziej rozpoznawalnym miejscem na tej wyspie byżo Azure Window, jednak w marcu 2017r. roku ten pićkny żuk skalny zawaliż sić do morza. Z Malty na Gozo można przepżynćć na wżasnć rćkć promem z Cirkewwa Ferry Terminal. 
+		Podobnie jest z odwiedzeniem Blue Lagune, na wyspie Comino. Podczas wycieczki cażodniowej kiedy zwiedzamy obie wyspy, na pobyt na Comino mamy tylko półtora godziny. Blue Lagune jest cudownym miejscem w ktżrym zdecydowanie warto spćdzić wićcej czasu.  Codziennie przypżywa tam bardzo dużo turystów, od rana zajmujć najlepsze miejsca wypoczynkowe. Warto wybrać sić w to miejsce wczeżniej, a pżynć tam wodne taksżwki ktżrymi można dotrzeć rankiem i wrżcić w momencie kiedy uznamy że mamy na to ochotć.</p>
+		<img src="<?php echo get_template_directory_uri();?>/img/IMAG1691_wm.jpg" width="840" height="480">
+		<p id="third">Wbrew ogólnej opinii uważam że Malta nie jest Tanim miejscem do wypoczynku. Można tam tanio dolecieć i znaleść tani nocleg jednak ceny wyżywienia sę dożć wysokie. Jeżli chcemy zjeżć w restauracji gdzież w pobliżu atrakcji turystycznej to musimy sić liczyć z tym, że cena dania obiadowego bćdzie wynosiża od 17 euro w gżrć. Do tego dochodzi jeszcze koszt jakiegoż picia i cena wzrasta do ponad 20 euro dla jednej osoby. Jeżli chcecie trochć zaoszczćdzić warto poszukać restauracji z dala od miejsc uczćszczanych przez turystżw. Mi udażo si a znależć bardzo miły lokal uczęszczany przez mieszkańców wyspy, gdzie serwowane byży potrawy typowo Maltażskie a ceny byży na poziomie 8 - 12 euro. Wićc warto przejżć sić po różnych dzielnicach i sprawdzić co oferujć lokalne restauracje.  
+		Na Malcie jest jeszcze bardzo dużo atrakcji które zdecydowanie trzeba odwiedzić i zobaczyć. Warto sić tam wybrać przynajmniej na caży tydzież żeby docenić tę wyspę. Mam nadziejć ze te kilka wskazżwek ktżre tutaj zaprezentoważem pozwolć wam lepiej zaplanować waszć wyprawć.<p>
+		<div  class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div id="f1_conteiner">
+						<div id="f1_card" class="shadow">
+							<div class="front face">
+								<img src="<?php echo get_template_directory_uri();?>/img/IMAG1677_wm.jpg" width="480" height="240">
+							</div>
+							<div class="back face center">
+								<p>This is nice for exposing more information about an image.</p>
+								<p>Any content can go here.</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-md-4"><img src="<?php echo get_template_directory_uri();?>/img/IMAG1684_wm.jpg" width="480" height="240"></div>
+				<div class="col-md-4"><img src="<?php echo get_template_directory_uri();?>/img/IMAG1691_wm.jpg" width="480" height="240"></div>
+			</div>
+		</div>
+		<div class="imgheader"> 
+			<div class="imgbackground">
+				<p>jdnkjnsfgknxflkzbnzxjfbnkcnklfnkljlknklnjkxzcn nnlkngklncxkjbnkjxznblkxzcnbl jnkjzcxnjkbnzcxkjb</p>
+			</div>
+		</div>
     </body>
 	<footer>
-	
-	
+		<div class="footercolor">
+			<li><button type="button" class="btn btn-default" aria-label="Left Align">
+				<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>
+				<p> Poznań jakiś tam adres </p>
+			</button>
+			<button type="button" class="btn btn-default" aria-label="Left Align">
+				<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+				<p> Adres @email <p>
+			</button>
+			<button type="button" class="btn btn-default" aria-label="Left Align">
+				<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+				<p> +48 505 728 427 <p>
+			</button></li>
+				<fieldset>
+					<legend>Dane do kontaktu</legend>
+					<li><label><input type="text" placeholder="Imię"></label>
+					<label><input type="text" placeholder="Nazwisko"></label>
+					<label><input type="text" placeholder="Adres @email"></label><li>
+				</fieldset>	
+		</div >
 	</footer>
 </html>
