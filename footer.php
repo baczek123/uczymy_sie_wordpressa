44841317a00c9470f2540b1c4754d5adc76026f2
baczek123 <bakowskipawel83@o2.pl>
2018-09-11 22:07:05 +0200

    po 4 filmikach

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 
         <footer>
             <p>To jest moja stopka</p>
+			<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
         </footer> 
         
         <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,13 +1,17 @@
 <?php
-function Uczymy_script_enqueue() {
 
-    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/UWP.css', array(), '1.0.0', 'all');
-    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/UWP.js', array(), '1.0.0', true);
-}
-add_action( 'wp_enqueue_scripts', 'Uczym_script_enqueue');
-
-function uczymuy_sie_suport() {
-add_theme_support('menus');
+function uczymy_script_enqueue() { 
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/index.css'  , array(),  '1.0.0', 'all');
+    wp_enqueue_script( 'customjs' , get_template_directory_uri() . '/js/index.js', array(), '1.0.0', true);
 }
 
-add_action('init', 'uczymuy_sie_suport');
+add_action( 'wp_enqueue_scripts', 'uczymy_script_enqueue');
+
+function uczymy_theme_setup() {
+    add_theme_support('menus');
+
+    register_nav_menu('primary', 'Primery Heder Navigation');  
+    register_nav_menu('secondary', 'Footer Navigation');
+}
+
+add_action('init', 'uczymy_theme_setup' );
