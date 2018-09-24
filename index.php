cc5a4d48c4eaa6e6facedb8a4ff9caede7c250f0
baczek123 <bakowskipawel83@o2.pl>
2018-09-24 18:16:59 +0200

    zaczęte arrey

--- index.php
+++ index.php
@@ -28,21 +28,52 @@ echo "$xy";
 ?>
 
 <?php
-if(5==5)
+if(5==6)
 {
     echo "5";
 }
 
-endif(6==6)
+elseif(6==7)
 {
-    echo "6"
+    echo "6";
 }
-endif(7==7)
+
+elseif (7==8)
 {
-    echo "7"
+    echo "7";
 }
 else
 {
-    echo "Wszystko jest źle"
+    echo "Wszystko jest źle";
 }
 ?>
+
+<?php
+
+$name='Tytus';
+switch($name)
+{
+    case'Paweł':
+    echo 'Witaj Pawle';
+    break;
+    case'Jakub':
+    echo 'Witaj Jakubie';
+    break;
+    default:
+    echo 'Nie masz dostempu do tej strony';
+    break;
+}
+?>
+
+<?php
+for($i=1; $i<=21; $i++)
+{
+echo $i;
+}
+?>
+
+<?php
+$Imiona=array('Jakub', 'Paweł', 'Sara', 'Michał', 'Maciej', 'Iwona', 'Konstanty' );
+echo $Imiona[4];
+?>
+
