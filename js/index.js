bb2216af2926c0e734ef3f3204c581d44755242c
baczek123 <bakowskipawel83@o2.pl>
2018-10-02 09:45:47 +0200

    połowa live strem 3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-//javascript functions
+jQuery(document).ready(function($) {
+
+    $(document).on('click','.open-search a', function(e) {
+        console.log('CLICKED ON THE OPEN SEARCH');
+    });
+
+});
