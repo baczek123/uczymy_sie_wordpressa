bf8835ede2dbbd78807f146abe49fb586c2526b3
baczek123 <bakowskipawel83@o2.pl>
2018-10-10 11:08:54 +0200

    formularz

--- index.php
+++ index.php
@@ -1,79 +1,17 @@
-<?php
-echo "cześć!";
-?>
+<!doctype html>
+<html>
 
-<?php
-define(NAME, "Paweł");
-echo "Cześć,".NAME;
-?>
+<head>
+  <meta charset="utf-8" />
+</head>
 
+<body>
+  <form action="wyniki.php" method="post">
+    <p>Kurtka zimowa, cena 120 zł, sztuk <input type="text" size="3" value="0" name="kurtka" /></p>
+    <p>Kalesony męskie, cena 40 zł, sztuk <input type="text" size="3" value="0" name="kalesony" /></p>
+    <p>Gacie na wacie, cena 60 zł, sztuk <input type="text" size="3" value="0" name="gacie" /></p>
+    <p><input type="submit" name="zakup" value="Kupuję podane ilości" /></p>
+  </form>
+</body>
 
-<?php
-$name="Paweł";
-echo "cześć jak si masz"."".$name;
-?>
-
-<?php
-
-$telefon=(5+5!==10)? "Prawda" : "Fałsz";
-echo "$telefon";
-?>
-
-<?php
-
-$x=6;
-$y=12;
-$xy=(2+4==$x)||(8+7<$y)? "Prawda" : "Fałsz";
-echo "$xy";
-?>
-
-<?php
-if(4==6)
-{
-    echo "5";
-}
-
-elseif(6==7)
-{
-    echo "6";
-}
-
-elseif (7==7)
-{
-    echo "7";
-}
-else
-{
-    echo "Wszystko jest źle";
-}
-?>
-
-<?php
-
-$name='Tytus';
-switch($name)
-{
-    case'Paweł':
-    echo 'Witaj Pawle';
-    break;
-    case'Jakub':
-    echo 'Witaj Jakubie';
-    break;
-    default:
-    echo 'Nie masz dostempu do tej strony';
-    break;
-}
-?>
-
-<?php
-for($i=1; $i<=21; $i++)
-{
-echo $i;
-}
-?>
-
-<?php
-$Imiona=array('Jakub', 'Paweł', 'Sara', 'Michał', 'Maciej', 'Iwona', 'Konstanty' );
-echo $Imiona[3];
-?>
-
+</html>
